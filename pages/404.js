a5c5061deea1a26917d921b71540e5e2a6ac6b24
d0tes <78929704+d0tes@users.noreply.github.com>
2022-10-24 23:02:44 +0800

    feat: add 404 page (#16)

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+export default function FourOhFour() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>404 Page</h1>
+    </div>
+  );
+}
