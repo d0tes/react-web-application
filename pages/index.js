62fd1156f2b0081019194e9617be92ce0732c0f8
END1502 <86548217+END1502@users.noreply.github.com>
2022-10-24 20:36:37 +0800

    feat: update title (#12)

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <Head>
+        <title>React Web Application</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1>Hello</h1>
+    </div>
+  );
 }
